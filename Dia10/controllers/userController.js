a2aeb04ff66715d08f878b67a85c70458e7706e2
pedrogomezcl <144635993+pedrogomezcl@users.noreply.github.com>
2025-08-21 12:43:15 +0000

    feat: :sparkles: Workshop P4

---
+++ Dia10/controllers/userController.js
@@ -0,0 +1,22 @@
+class UsuarioController{
+    constructor(modelo,vista){
+        this.modelo=modelo;
+        this.vista=vista;
+    }
+    async registrarUsuario(){
+        const datos = this.vista.pedirDatosUsuario();
+        try{
+            const id = await this.modelo.crear(datos);
+            this.vista.mostrarMensaje(`Usuario creado con ID: ${id}`);
+        }catch(error){
+            this.vista.mostrarMensaje(`Error: ${error.message}`);
+        }
+    }
+
+    async mostrarUsuarios(){
+        const usuarios = await this.modelo.listar();
+        this.vista.mostrarUsuarios(usuarios);
+    }
+
+}
+module.exports=UsuarioController;
